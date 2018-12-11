e3f5f5c2df086f4046d3017644aed49f409694cb
Guilherme Franco <guilherme.franco@terra.com.br>
2018-12-11 14:18:25 -0200

    Acrescentando comentarios aos posts

--- src/main/java/com/guifranco/workshopmongo/config/Inicializacao.java
+++ src/main/java/com/guifranco/workshopmongo/config/Inicializacao.java
@@ -1,5 +1,6 @@
 package com.guifranco.workshopmongo.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Date;
 
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.guifranco.workshopmongo.dominios.Post;
 import com.guifranco.workshopmongo.dominios.User;
 import com.guifranco.workshopmongo.dto.AutorDTO;
+import com.guifranco.workshopmongo.dto.CommentDTO;
 import com.guifranco.workshopmongo.repositorios.PostRepositorio;
 import com.guifranco.workshopmongo.repositorios.UserRepositorio;
 
@@ -25,7 +27,7 @@ public class Inicializacao implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
-	//	SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 	//	sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 		Date dataHoraAgora = new Date();
 		
@@ -39,8 +41,15 @@ public class Inicializacao implements CommandLineRunner {
 		userRepositorio.saveAll(Arrays.asList(maria, alex, bob));
 		
 		Post post1 = new Post(null, new AutorDTO(maria),  dataHoraAgora, "Partiu viagem", "Vou viajar para São Paulo. Abraços!");
-		Post post2 = new Post(null, new AutorDTO(maria),  dataHoraAgora, "Bom dia", "Acordei feliz hoje!");
+		Post post2 = new Post(null, new AutorDTO(maria),  sdf.parse("12/12/2018"), "Bom dia", "Acordei feliz hoje!");
 				
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", dataHoraAgora, new AutorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("12/12/2018"), new AutorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia", dataHoraAgora, new AutorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
+		
 		postRepositorio.saveAll(Arrays.asList(post1, post2));
 		
 		maria.getPosts().addAll(Arrays.asList(post1, post2));
--- src/main/java/com/guifranco/workshopmongo/dominios/Post.java
+++ src/main/java/com/guifranco/workshopmongo/dominios/Post.java
@@ -1,12 +1,15 @@
 package com.guifranco.workshopmongo.dominios;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.guifranco.workshopmongo.dto.AutorDTO;
+import com.guifranco.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -19,6 +22,8 @@ public class Post implements Serializable {
 	private Date data;
 	private String titulo;
 	private String msg;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 		
 	public Post() {
 	}
@@ -75,6 +80,14 @@ public class Post implements Serializable {
 		return serialVersionUID;
 	}
 
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
